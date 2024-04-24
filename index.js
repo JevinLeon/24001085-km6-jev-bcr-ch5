08398e6a02d15974eb4627d320633ea605076dee
JevinLeon <jevinleon@gmail.com>
2024-04-24 20:19:34 +0700

    Use cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const express = require("express");
@@ -8,6 +9,7 @@ const router = require("./routes");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
